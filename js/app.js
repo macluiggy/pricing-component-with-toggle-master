d488705c2039b3dd60c66a091a078d6c601f073d
Luiggy Macias <ferrinluiggy@gmail.com>
2021-04-21 11:48:15 -0500

    pilas

---
+++ js/app.js
@@ -0,0 +1,33 @@
+(function () {
+	var checkbox = document.querySelector('#checkbox');
+	var anual = document.querySelectorAll('.anual');
+	var mensual = document.querySelectorAll('.mensual');
+	var prices = document.querySelectorAll('.prices');
+
+	checkbox.addEventListener('input', function () {
+		for(let i = 0, length1 = prices.length; i < length1; i++){
+			if (checkbox.checked) {
+			// statement
+			anual[i].className = 'show';
+			mensual[i].className = 'hide';
+		} else {
+			// statement
+			anual[i].className = 'hide';
+			mensual[i].className = 'show';
+		}
+		}
+		/*prices.forEach( function(price) {
+			// statements
+			if (checkbox.checked) {
+				// statement
+				price.firstChild.className = 'show';
+				price.lastChild.className = 'hide';
+			} else {
+				// statement
+				price.firstChild.className = 'hide';
+				price.lastChild.className = 'show';
+			}
+		});*/
+		
+	}, false)
+}());
